7d4a993d946b609f60d8d930331cd47eeb8ceef5
asianvader <phoebe.voong@gmail.com>
2020-12-20 14:49:00 +0000

    Add Header component and CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from "react";
+import Header from "./components/ui/Header";
 
 function App() {
   return (
     <div className="container">
-      hello
+      <Header />
+
     </div>
   );
 }
