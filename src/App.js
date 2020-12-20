24c21b4bff63efb5ffa4f0e25e290af7f6e637f1
asianvader <phoebe.voong@gmail.com>
2020-12-20 15:13:59 +0000

    Use axios to call api endpoint and store data in an array.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Header from "./components/ui/Header";
+import axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    }
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
-
     </div>
   );
 }
